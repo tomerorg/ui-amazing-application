4b44ad79b730dc50f824d402cd25445f2c7ef3a2
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 10:00:08 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ui-amazing-application",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="A Python ui implementation using NumPy",
+    keywords="ui-amazing-application, python",
+    url="",
+)
